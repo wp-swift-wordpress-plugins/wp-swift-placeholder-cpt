bdcce0d0f58a20aea39da6166120741dfb3433a5
Gary Swift <garyswiftmail@gmail.com>
2018-06-22 10:19:35 +0100

    Added shortcodes

--- functions/html.php
+++ functions/html.php
@@ -1,8 +1,8 @@
 <?php
-function wp_prefix_placeholder_html() {
+function wp_prefix_placeholder_html( $atts = array() ) {
 	return wp_prefix_placeholders_get_template_part( 'html/the-content-single.php', true );
 }
-function wp_prefix_placeholders_html() {
+function wp_prefix_placeholders_html( $atts = array() ) {
 	return wp_prefix_placeholders_get_template_part( 'html/placeholder-loop.php' );
 }
 function wp_prefix_placeholders_get_template_part( $template, $single = false, $global = false ) {
--- functions/shortcodes.php
+++ functions/shortcodes.php
@@ -1,2 +1,19 @@
 <?php
-// Silence is golden.
+/**
+ * Shortcodes
+ *
+ * If the CPT want to use shortcodes
+ *
+ * @author  		Gary Swift <gary@brightlight.ie>
+ *
+ * @since 			1.0
+ */
+
+// [placeholder foo="foo-value"]
+function placeholder_func( $atts ) {
+    return wp_prefix_placeholder_html( $atts );
+}
+// [placeholders foo="foo-value"]
+function placeholders_func( $atts ) {
+    return wp_prefix_placeholders_html( $atts );
+}
